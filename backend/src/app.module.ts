1bb31e55069a86f6f6b571ffd6d5bc6b6f721769
Miłosz Szekiel <mszekiel@outlook.com>
2020-02-13 19:55:10 +0100

    add serve static

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { HideoutController } from './controllers/hideout.controller';
 import { HideoutProvider } from './providers/item.provider';
 import { HideoutService } from './services/hideout.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: resolve('../../application/dist'),
+    }),
+  ],
   controllers: [HideoutController],
   providers: [HideoutProvider, HideoutService],
 })
