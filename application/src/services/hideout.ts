f080e759cda9e281da8749ebf62fa45090293208
Miłosz Szekiel <mszekiel@outlook.com>
2020-02-13 19:41:50 +0100

    fix typo

--- application/src/services/hideout.ts
+++ application/src/services/hideout.ts
@@ -1,7 +1,7 @@
 import { Recipe } from "../types/Recipe";
 
 export function getRecipes() {
-  return fetch(`https://tarkov-calculator.herokuapp.com/:2137/hideout/recipes`)
+  return fetch(`https://tarkov-calculator.herokuapp.com:2137/hideout/recipes`)
     .then(response => response.json())
     .then((result:Recipe[]) => result);
 }
